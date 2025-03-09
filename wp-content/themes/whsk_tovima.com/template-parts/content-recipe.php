fbe783d8eabbf4321822ccfc4123049c90dcbd8f
maryoikonomou33 <std154998@ac.eap.gr>
2025-03-09 17:32:23 +0200

    Second commit, added some style

--- wp-content/themes/whsk_tovima.com/template-parts/content-recipe.php
+++ wp-content/themes/whsk_tovima.com/template-parts/content-recipe.php
@@ -13,14 +13,11 @@ get_header();
             the_post();
 
             $post_id = get_the_ID();
-
             $field_groups = acf_get_field_groups(array('post_id' => $post_id));
 
-            
             $info_fields = [];
             $recipe_fields = [];
 
-            
             foreach ($field_groups as $field_group) {
                 if ($field_group['title'] == 'Info') {
                     $info_fields = acf_get_fields($field_group['ID']);
@@ -29,12 +26,13 @@ get_header();
                 }
             }
 
-            
             $prep_time = get_field('prep_time') ?? 'Δεν είναι διαθέσιμο'; 
             $cook_time = get_field('cook_time') ?? 'Δεν είναι διαθέσιμο'; 
             $difficulty = get_field('difficulty') ?? '0'; 
             $ingredients = get_field('ingredients') ?? ''; 
             $steps = get_field('steps') ?? [];
+            $tip = get_field('to_tipaki') ?? 'Δεν είναι διαθέσιμο';
+            $source = get_field('recipe_source') ?? 'Δεν είναι διαθέσιμη';
 
             $description = get_the_excerpt();
         ?>
@@ -49,8 +47,7 @@ get_header();
             <h1 class="recipe-title"><?php the_title(); ?></h1>
             <p class="recipe-description"><?php echo esc_html($description); ?></p>
             <div class="recipe-author">
-            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
-
+                <img src="https://via.placeholder.com/150" alt="Placeholder Image">
                 <p>Αργυρώ Μπαρμπαρίγου</p>
             </div>
         </div>
@@ -59,29 +56,23 @@ get_header();
             <?php if ( has_post_thumbnail() ) : ?>
                 <?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?>
             <?php endif; ?>
-
-            
         </div>
     </div>
-
 </div>
 
 <hr class="custom-divider">
 
-
 <div class="recipe-grid">
-
+    
     <div class="recipe-sidebar">
         <div class="recipe-info">
-        <div class="info-divider"></div>
+            <div class="info-divider"></div>
             <h2 class="info-title">Info</h2>
-            <div class="info-dividergray"></div>
-            <p class="info-text"><strong>Χρόνος Προετοιμασίας:</strong> <?php echo esc_html($prep_time); ?> </p>
-            <p class="info-text"><strong>Χρόνος Μαγειρέματος:</strong> <?php echo esc_html($cook_time); ?> </p>
-            <p class="info-text"><strong>Βαθμός Δυσκολίας:</strong> 
+            <p class="info-text">Χρόνος Προετοιμασίας: <?php echo esc_html($prep_time); ?></p>
+            <p class="info-text">Χρόνος Μαγειρέματος: <?php echo esc_html($cook_time); ?></p>
+            <p class="info-text">Βαθμός Δυσκολίας:
                 <?php 
                 $difficulty_value = intval($difficulty);
-                
                 if ($difficulty_value > 0) {
                     for ($i = 0; $i < $difficulty_value; $i++) {
                         echo '<img src="' . get_template_directory_uri() . '/assets/images/knife.svg" alt="Knife Icon" class="knife-icon">';
@@ -97,7 +88,6 @@ get_header();
         </div>
 
         <div class="recipe-ingredients">
-        <div class="ingr-divider"></div>
             <h2 class="ingredients-title">Υλικά</h2>
             <?php if( !empty($ingredients) ): ?>
                 <div class="ingredients-list">
@@ -109,23 +99,45 @@ get_header();
         </div>
     </div>
 
-    <div class="recipe-steps">
-    <div class="steps-divider"></div>
-        <h2 class="steps-title">Εκτέλεση</h2>
+    
+    <div class="recipe-content">
+        <div class="recipe-steps">
+            <div class="steps-divider"></div>
+            <h2 class="steps-title">Εκτέλεση</h2>
+            <?php if( have_rows('steps') ): ?>
+                <ol>
+                    <?php while( have_rows('steps') ): the_row(); ?>
+                        <li><?php the_sub_field('step'); ?></li>
+                    <?php endwhile; ?>
+                </ol>
+            <?php else : ?>
+                <p class="info-text">Δεν υπάρχουν διαθέσιμα βήματα εκτέλεσης.</p>
+            <?php endif; ?>
+        </div>
+
         
-        <?php if( have_rows('steps') ): ?>
-            <ol>
-                <?php while( have_rows('steps') ): the_row(); ?>
-                    <li><?php the_sub_field('step'); ?></li>
-                <?php endwhile; ?>
-            </ol>
-        <?php else : ?>
-            <p class="info-text">Δεν υπάρχουν διαθέσιμα βήματα εκτέλεσης.</p>
-        <?php endif; ?>
+        <div class="recipe-extra">
+            <div class="tipaki-wrapper">
+                <h2 class="tipaki-title"># το tipακι</h2>
+                <?php if( !empty($tip) ): ?>
+                    <div class="tipaki-list">
+                        <?php echo wp_kses_post($tip); ?>
+                    </div>
+                <?php else : ?>
+                    <p class="info-text">Δεν υπάρχει.</p>
+                <?php endif; ?>
+            </div>
+
+            <div class="recipe-source">
+                <p>Πηγή Συνταγής: <?php echo esc_html($source); ?></p>
+            </div>
+        </div>
     </div>
 
+    
     <div class="recipe-placeholder"></div>
 </div>
+
 
         <?php }
     } ?>
